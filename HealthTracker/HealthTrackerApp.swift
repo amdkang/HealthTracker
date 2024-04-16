87c256874fc1dfefadee0ea2e6a34a33a993c6d9
Amenda Kang <amendakang@gmail.com>
2024-04-15 21:39:10 -0400

    Initial Commit

---
+++ HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Amenda Kang on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
